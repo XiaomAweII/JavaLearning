6eae5d9c53cd1d1522779c85fee2981a67b03d0a
XiaomAweII <805122541@qq.com>
2025-02-20 21:49:07 +0800

    Java核心技术卷：第二章-Java编程环境 当中出现的代码

---
+++ JavaCore/Chapter02/01Welcome/src/Welcome.java
@@ -0,0 +1,14 @@
+/**
+ * @author xiaoweii
+ * @create 2025-02-20 21:13
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        String greeting = "Welcome to Core Java!";
+        System.out.println(greeting);
+        for (int i = 0; i < greeting.length(); i++) {
+            System.out.print("=");
+        }
+        System.out.println();
+    }
+}
